954b2162ae62da3099af230d28db0dd85de9dac7
AzharIftekhari <iftekhari.azhar86@gmail.com>
2026-03-27 11:02:20 +0530

    cloud security project

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,3 @@
-#key pair
-resource "aws_key_pair" "securecart-key" {
-  key_name = "securecart-key-ec2"
-  public_key = file("terra-key-ec2.pub")
-}
-
-#Bastion App
-resource "tls_private_key" "bastion-instance-key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "bastion-key-pair" {
-  key_name   = "bastion-instance-key"
-  public_key = tls_private_key.bastion-instance-key.public_key_openssh
-}
-
-resource "local_file" "bastion_pem" {
-  content  = tls_private_key.bastion-instance-key.private_key_pem
-  filename = "bastion-instance-key.pem"
-  file_permission = "0400"
-}
 
 #Instance_Public
 resource "aws_instance" "bastion-host" {
@@ -35,22 +13,7 @@ resource "aws_instance" "bastion-host" {
   }
 }
 
-#Securecart-App
-resource "tls_private_key" "securecart-app-key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
 
-resource "aws_key_pair" "private-key-pair" {
-  key_name   = "private-instance-key"
-  public_key = tls_private_key.securecart-app-key.public_key_openssh
-}
-
-resource "local_file" "private-pem" {
-  content  = tls_private_key.securecart-app-key.private_key_pem
-  filename = "securecart-key.pem"
-  file_permission = "0400"
-}
 
 #Instance_Private
 resource "aws_instance" "securecart-app" {
